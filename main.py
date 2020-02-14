920124ea18507c92f060cb7648fba8de36840acf
Bahaa Hussein <bhhuss79>
2020-02-14 10:07:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
-import pygame # import library
+import pygame
 pygame.init()
 
-# Create the window
-win = pygame.display.set_mode((1000, 1000))
+win = pygame.display.set_mode((800, 600))
+img = pygame.image.load('assets/forest-assets/door.png').convert()
+
+# Load the spritesheet
+spritesheet = pygame.image.load('assets/gfx/objects.png').convert()
+
+
+# Create the first image
+img = pygame.Surface([100,100]).convert()
+img.blit(spritesheet, (0, 0), (0, 0, 16, 16))
+
+# Create the second image
+img = pygame.Surface([16, 16]).convert()
+img.blit(spritesheet, (0, 0), (16, 0, 16, 16))
 
 run = True
 while run:
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
 
-# Game code starts here ---------------------
-  win.fill((0, 0, 0))
- 
-  # Draw a rectangle
-  pygame.draw.rect(win, (0, 204, 255), (50, 50, 100, 200))
-  
-  #Update the display
-img = pygame.image.load("cave.png").convert 
-img = pygame.image.load("assets/gfx/cave.png").convert()
-print 
-img = pygame.image.load("character.png").convert
-img = pygame.image.load("assets/gfx/character.png").convert()
-print
-img = pygame.image.load("front.png").convert
-img = pygame.image.load("assets/gfx/front.png").convert()
-print 
-img = pygame.image.load("Inner.png").convert
-img = pygame.image.load("assets/gfx/Inner.png").convert()
-print 
-img = pygame.image.load("Log.png").convert
-img = pygame.image.load("assets/gfx/Log.png").convert()
-pygame.display.update()
-
-
-print("Ending game")
+    win.fill((0, 0, 0))
+    win.blit(img, (400, 300))
+    win.blit(img, (100, 100))
+    win.blit(img, (150, 100))
