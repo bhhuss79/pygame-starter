c5f3515d1ab955f156c85ae1608424cc353272ef
Bahaa Hussein <bhhuss79>
2020-02-24 11:18:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,101 +1,15 @@
-import pygame
-pygame.init()
-
-win = pygame.display.set_mode((800, 900))
-img = pygame.image.load('assets/forest-assets/door.png')
-
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-keys=pygame.key.get_pressed()
-
-win.fill((0, 0, 0))
-win.blit(img, (400, 300))
-x = 200
-y= 200
-if keys[pygame.K_LEFTBRACKET]:
-     x-=1
-if keys[pygame.K_RIGHTBRACKET]:
-  x += 1
-
-
-
-pygame.display.update()
-
-import curses
-from curses import KEY_RIGHT, KEY_LEFT, KEY_UP, KEY_DOWN
-from random import randint
-
-
-curses.initscr()
-win = curses.newwin(20, 60, 0, 0)
-win.keypad(1)
-curses.noecho()
-curses.curs_set(0)
-win.border(0)
-win.nodelay(1)
-
-key = KEY_RIGHT                                                    # Initializing values
-score = 0
-
-snake = [[4,10], [4,9], [4,8]]                                     # Initial snake co-ordinates
-food = [10,20]                                                     # First food co-ordinates
-
-win.addch(food[0], food[1], '*')                                   # Prints the food
-
-while key != 27:                                                   # While Esc key is not pressed
-    win.border(0)
-    win.addstr(0, 2, 'Score : ' + str(score) + ' ')                # Printing 'Score' and
-    win.addstr(0, 27, ' SNAKE ')                                   # 'SNAKE' strings
-    win.timeout(150 - (len(snake)/5 + len(snake)/10)%120)          # Increases the speed of Snake as its length increases
-    
-    prevKey = key                                                  # Previous key pressed
-    event = win.getch()
-    key = key if event == -1 else event 
-
-
-    if key == ord(' '):                                            # If SPACE BAR is pressed, wait for another
-        key = -1                                                   # one (Pause/Resume)
-        while key != ord(' '):
-            key = win.getch()
-        key = prevKey
-        continue
-
-    if key not in [KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN, 27]:     # If an invalid key is pressed
-        key = prevKey
-
-    # Calculates the new coordinates of the head of the snake. NOTE: len(snake) increases.
-    # This is taken care of later at [1].
-    snake.insert(0, [snake[0][0] + (key == KEY_DOWN and 1) + (key == KEY_UP and -1), snake[0][1] + (key == KEY_LEFT and -1) + (key == KEY_RIGHT and 1)])
-
-    # If snake crosses the boundaries, make it enter from the other side
-    if snake[0][0] == 0: snake[0][0] = 18
-    if snake[0][1] == 0: snake[0][1] = 58
-    if snake[0][0] == 19: snake[0][0] = 1
-    if snake[0][1] == 59: snake[0][1] = 1
-
-    # Exit if snake crosses the boundaries (Uncomment to enable)
-    #if snake[0][0] == 0 or snake[0][0] == 19 or snake[0][1] == 0 or snake[0][1] == 59: break
-
-    # If snake runs over itself
-    if snake[0] in snake[1:]: break
+print("Hello oliver, you are about to wake up to start your journey")
+print("The sound of your alarm startles you, you wake up drenched in sweat.")
+print("You get dressed for work and head to your car.")
+print("  ______ \n /|_||_\`.__\n(   _    _ _\ \n=`-(_)--(_)-'  hjw)") # this is a car
+print("Your car doesn't start! The only way to go to work is to walk you can't be late to work")
+choice = input("You keep walking and remember there is a shortcut in the forest. Do you A) go in the forest because its faster or B) go the normal way because its safer but longer?")
+if choice == "A" :
+     print("As you are walking trough the forest, you notice that the grass is very tall, no one has been here for a while... ")
+     print("As you are walking, you hear a stick click behind you as if someone stepped on it. Your mind thinks: RUN! So you run as fast as you can never looking back.")
+     print ("You get out of the forest. You feel so blessed that whatever that was didn't catch you, you are now on the road and you keep walking, terified")
+     print (" _O/ \n  \ \n  /\_ \n \  ` \n  ` \n  ")
+else:
+       print("You keep walking on the road, and notice that no one is on the road, its just you, where is everybody...")
 
     
-    if snake[0] == food:                                            # When snake eats the food
-        food = []
-        score += 1
-        while food == []:
-            food = [randint(1, 18), randint(1, 58)]                 # Calculating next food's coordinates
-            if food in snake: food = []
-        win.addch(food[0], food[1], '*')
-    else:    
-        last = snake.pop()                                          # [1] If it does not eat the food, length decreases
-        win.addch(last[0], last[1], ' ')
-    win.addch(snake[0][0], snake[0][1], '#')
-    
-curses.endwin()
-print("\nScore - " + str(score))
-print("http://bitemelater.in\n")
