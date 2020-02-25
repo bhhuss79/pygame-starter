cca4fb78f4e331acc517b6ff8eb8c2d9b5286751
Bahaa Hussein <bhhuss79>
2020-02-25 11:15:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
+import time
+
+def clear(): 
+    from os import system, name 
+    # for windows 
+    if name == 'nt': 
+        _ = system('cls') 
 print("Hello oliver, you are about to wake up to start your journey")
 print("The sound of your alarm startles you, you wake up drenched in sweat.")
+time. sleep(10.)
 print("You get dressed for work and head to your car.")
-print("  ______ \n /|_||_\`.__\n(   _    _ _\ \n=`-(_)--(_)-'  hjw)") # this is a car
+print("  ______ \n /|_||_\`.__\n(   _    _ _\ \n=`-(_)--(_)-' )") # this is a car
+print("Your car is not working, you try one more time...")
+print("   (`  ).                   _  \n   (     ).              .:(`  )`.  \n  _(       '`.          :(   .    ) \n  .=(`(      .   )     .--  `.  (    ) ) \n  ((    (..__.:'-'   .+(   )   ` _`  ) ) \n      `(       ) )       (   .  )     (   )  ._   /n
+  )      ` __.:'   )     (   (   ))     `-'.-(`  ) 
+)  )  ( )       --'       `- __.'         :(      )) 
+.-'  (_.'          .')                    `(    )  ))
+                  (_  )                     ` __.:'          
+                                        
+--..,___.--,--'`,---..-.--+--.,,-,,..._.--..-._.-a:f--.")
+
 print("Your car doesn't start! The only way to go to work is to walk you can't be late to work")
 choice = input("You keep walking and remember there is a shortcut in the forest. Do you A) go in the forest because its faster or B) go the normal way because its safer but longer?")
 if choice == "A" :
      print("As you are walking trough the forest, you notice that the grass is very tall, no one has been here for a while... ")
      print("As you are walking, you hear a stick click behind you as if someone stepped on it. Your mind thinks: RUN! So you run as fast as you can never looking back.")
      print ("You get out of the forest. You feel so blessed that whatever that was didn't catch you, you are now on the road and you keep walking, terified")
-     print (" _O/ \n  \ \n  /\_ \n \  ` \n  ` \n  ")
+     time.sleep(15.)#wait for 15 seconds so you can read the text above 
+     clear()
+     print (" _O/ \n  \ \n  /\_ \n \  ` \n  ` \n  ")  #this is a person running 
+     time.sleep(1.) # wait for 300 milliseconds
+     clear()
+     print ("   , \n  / \n `\_\ \n  \ \n /O\ \n  ")
+     time.sleep(1.)  #wait for 300 milliseconds 
+
+          
+          
 else:
        print("You keep walking on the road, and notice that no one is on the road, its just you, where is everybody...")
 
